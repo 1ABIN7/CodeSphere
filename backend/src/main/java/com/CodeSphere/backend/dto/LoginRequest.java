fd7a17a41dde87d5070c539cddcb57c73b5d8266
RahulSatishkr <rahul.satishkr@gmail.com>
2026-07-24 09:46:35 -0500

    updated settings tweaks to get login to work

--- backend/src/main/java/com/CodeSphere/backend/dto/LoginRequest.java
+++ backend/src/main/java/com/CodeSphere/backend/dto/LoginRequest.java
@@ -1,7 +1,7 @@
 package com.CodeSphere.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
@@ -12,7 +12,7 @@ import lombok.*;
 @Builder
 public class LoginRequest {
 
-    @JsonProperty("username")
+    @JsonAlias({"username", "email", "usernameOrEmail"})
     @NotBlank(message = "Username or email is required")
     private String usernameOrEmail;
 
